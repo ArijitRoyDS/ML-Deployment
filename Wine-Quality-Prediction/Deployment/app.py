6dc338394a59d7194acffc66d6d75818676df617
Arijit Roy Chowdhury <72007160+ArijitRoyDS@users.noreply.github.com>
2023-06-11 02:48:27 +0530

    Add files via upload

--- Wine-Quality-Prediction/Deployment/app.py
+++ Wine-Quality-Prediction/Deployment/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import pickle
+import sklearn
 
 app = Flask(__name__)
 
@@ -7,17 +9,46 @@ app = Flask(__name__)
 def base():
     return render_template('home.html')    # Reads 'home.html' from templates folder
 
-@app.route('/contact')
-def contact():
-    return 'Contact Page'
+@app.route('/predict', methods=['post'])
+def predict():
+    # Desired Inputs: 'volatile_acidity', 'residual_sugar', 'chlorides', 'total_sulfur_dioxide', 'ph', 'sulphates', 'alcohol'
+    # Get the inputs from home.html
+    volatile_acidity = request.form.get('volatile_acidity')
+    residual_sugar = request.form.get('residual_sugar')
+    chlorides = request.form.get('chlorides')
+    total_sulfur_dioxide = request.form.get('total_sulfur_dioxide')
+    ph = request.form.get('ph')
+    sulphates = request.form.get('sulphates')
+    alcohol = request.form.get('alcohol')
 
-@app.route('/gallery')
-def gallery():
-    return render_template('gallery.html')
+    print("volatile_acidity: ", volatile_acidity)
+    print("residual_sugar: ", residual_sugar)
+    print("chlorides: ", chlorides)
+    print("total_sulfur_dioxide: ", total_sulfur_dioxide)
+    print("ph: ", ph)
+    print("sulphates: ", sulphates)
+    print("alcohol: ", alcohol)
 
-@app.route('/cart')
-def cart():
-    return 'Cart Page'
+    # Call the Model
+    model = pickle.load(open('wine_quality_prediction_80.pkl', 'rb'))
+    output = model.predict([[volatile_acidity, residual_sugar, chlorides, total_sulfur_dioxide, ph, sulphates, alcohol]])
+    print(output)
+    data = "The quality of wine on a scale of 1-10 is: " + str(output[0])
+
+    return render_template('predict.html', data = data)
+
+
+# @app.route('/contact')
+# def contact():
+#     return 'Contact Page'
+
+# @app.route('/predict')
+# def predict():
+#     return render_template('predict.html')
+
+# @app.route('/cart')
+# def cart():
+#     return 'Cart Page'
 
 
 
